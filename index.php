2d71dafbf92e5216f831f7a01da0d98bea551ede
Amit Sinha <amitsinhadeveloper@gmail.com>
2025-07-05 12:03:16 +0530

    wprked on fix the router

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ error_reporting(E_ALL);
 require_once __DIR__ . '/src/Router.php';
 require_once __DIR__ . '/src/controllers/DashboardController.php';
 
+use Src\Router;
 use Src\Controllers\DashboardController;
 
 $router = new Router();
