1aa7e80e1febc20d4f33c831202d102d65730394
Amit Sinha <amitsinhadeveloper@gmail.com>
2025-07-05 12:06:15 +0530

    Router now exists

--- src/Router.php
+++ src/Router.php
@@ -1,4 +1,8 @@
-class Router {
+<?php
+namespace Src;
+
+class Router
+{
     private $getRoutes = [];
 
     public function get($path, $callable) {
@@ -6,22 +10,17 @@ class Router {
     }
 
     public function run() {
-        // strip query-string, etc.
         $uri    = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
         $method = $_SERVER['REQUEST_METHOD'];
 
         if ($method === 'GET' && isset($this->getRoutes[$uri])) {
             $callable = $this->getRoutes[$uri];
 
-            // if the route is [ 'Fully\Qualified\ControllerName', 'method' ]
             if (is_array($callable) && is_string($callable[0])) {
-                // instantiate the controller
                 $controller = new $callable[0];
-                // replace the class name with the instance
                 $callable   = [ $controller, $callable[1] ];
             }
 
-            // now call it
             return call_user_func($callable);
         }
 
